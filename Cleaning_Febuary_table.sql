2605dd832f0665f33c5af7b7a972dca656f4e283
Olan1ke <127860303+Olan1ke@users.noreply.github.com>
2024-02-11 17:15:36 +0100

    Update Cleaning_Febuary_table.sql

--- Cleaning_Febuary_table.sql
+++ Cleaning_Febuary_table.sql
@@ -16,11 +16,4 @@ from Sales_February_2019 ;
   
   Select * From sales_feb_2019;
 
- -- 2. Populate the table
-
- Select a.order_id, a.Price_Each,b.order_id,b.Price_Each
- From sales_feb_2019 a
- join sales_feb_2019 b
- on a.order_id=b.order_id
- And a.Price_Each <> b.Price_Each
-Where a.order_id = ' ';
+ 
